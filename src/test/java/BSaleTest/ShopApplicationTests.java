da16f7700d4cf8e8edae5dd611e62680f707e424
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2021-05-06 15:15:48 -0400

    Initial_Commit

---
+++ src/test/java/BSaleTest/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package BSaleTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
